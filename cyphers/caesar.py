a249f4abc2fc3a5301868d38b4d53060eb1a1d13
niyolu <nijolu@live.de>
2022-10-10 15:20:08 +0200

    refactor, decrypt reduces encrypt

--- cyphers/caesar.py
+++ cyphers/caesar.py
@@ -3,12 +3,9 @@ from alphabets.latin import Latin
     
 class Caesar(CryptoBase):
     def encrypt(x: str, n: int = 3) -> str:
-        def encrypt_char(c: chr):
+        def encrypt_char(c: str):
             return Latin.chr((Latin.ord(c) + n) % len(Latin))
         return "".join([encrypt_char(c) if c != Latin.delim else c for c in x])
-    
+
     def decrypt(x: str, n: int = 3) -> str:
-        def decrypt_char(c: chr):
-            return Latin.chr((Latin.ord(c) - n) % len(Latin))
-        return "".join([decrypt_char(c) if c != Latin.delim else c for c in x])
-    
+        return Caesar.encrypt(x, -n)
