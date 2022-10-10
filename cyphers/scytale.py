6466f818b1722d5e9923d10b795ae669da3a2d0d
niyolu <nijolu@live.de>
2022-10-10 15:20:20 +0200

    numpy version

--- cyphers/scytale.py
+++ cyphers/scytale.py
@@ -1,4 +1,5 @@
 from cyphers.base import CryptoBase
+import numpy as np
 
 # scytale as a matrix; unraveled column wise
 #   H | O | W
@@ -15,18 +16,21 @@ from cyphers.base import CryptoBase
 
 
 class Scytale(CryptoBase):
+    def encrypt(x, w):
+        return "".join(np.array(list(x)).reshape((len(x) // w, w)).T.ravel())
+    
     def encrypt2(x, w):
         n = len(x)
         assert n % w == 0
         h = n // w
         cypher = ["-"] * n
-        for i in range(n):
+        for i, c in enumerate(x):
             row = i // w
             col = i % w
-            cypher[row + col * h] = x[i]
+            cypher[row + col * h] = c
         return "".join(cypher)
     
-    def encrypt(x, w):
+    def encrypt3(x, w):
         n = len(x)
         assert n % w == 0
         h = n // w
@@ -39,8 +43,10 @@ class Scytale(CryptoBase):
     
     def decrypt(x, w):
         return Scytale.encrypt(x, len(x) // w)
+
     
 def visualize(x, h, w):
     matrix = []
     for j in range(h):
         print(x[j: j + h * w: h])
+
