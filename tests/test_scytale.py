de1cd27941c0ff628c03661a1ac3ed72ffcd967b
niyolu <nijolu@live.de>
2022-10-10 15:20:37 +0200

    ??

--- tests/test_scytale.py
+++ tests/test_scytale.py
@@ -1,12 +1,13 @@
 from cyphers.scytale import Scytale, visualize
 from tests.testutils import test_cypher
 
-def test():
+def test2():
     msg = "HOWDOESTHISWORK"
     w, h = 3, 5
     test_cypher(Scytale.encrypt, Scytale.decrypt, msg, w)
     visualize(Scytale.encrypt(msg, w), h, w)
     visualize(Scytale.decrypt(Scytale.encrypt(msg, w), w), w, h)
+
     
 if __name__ == "__main__":
     test()
