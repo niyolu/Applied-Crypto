3dd724113c60fd2c75e7adde61ebcf598be676b8
niyolu <nijolu@live.de>
2022-10-13 04:33:58 +0200

    added scytale benchmark

--- tests/test_scytale.py
+++ tests/test_scytale.py
@@ -1,13 +1,22 @@
 from cyphers.scytale import Scytale, visualize
-from tests.testutils import test_cypher
+from tests.testutils import test_cypher, bench_cypher
 
-def test2():
+
+def test():
     msg = "HOWDOESTHISWORK"
     w, h = 3, 5
-    test_cypher(Scytale.encrypt, Scytale.decrypt, msg, w)
+    test_cypher(Scytale.encrypt, Scytale.decrypt, msg, w, verbose=True)
+    print("cypher visualization:")
     visualize(Scytale.encrypt(msg, w), h, w)
     visualize(Scytale.decrypt(Scytale.encrypt(msg, w), w), w, h)
+    
+def bench():
+    msg = "HOWDOESTHISWORK"*100
+    w = len(msg) // 5
+    return bench_cypher(Scytale.encrypt, Scytale.decrypt, msg, w, name="scytale", verbose=True)
 
     
 if __name__ == "__main__":
     test()
+    print()
+    bench()
