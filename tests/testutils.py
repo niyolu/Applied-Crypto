0ada4aa636d4e004edcb2b4e97dee0f7e9decdda
niyolu <nijolu@live.de>
2022-10-13 04:33:48 +0200

    added cypher benchmarking

--- tests/testutils.py
+++ tests/testutils.py
@@ -1,6 +1,33 @@
-def test_cypher(enc, dec, msg, *args):
-    encryped = enc(msg, *args)
-    decrypted = dec(encryped, *args)
-    print(msg, encryped, decrypted, sep="\n|\nV\n")
+import timeit
+from functools import partial
+
+
+def test_cypher(enc, dec, msg, *args, name="Cypher", verbose=True):
+    encrypted = enc(msg, *args)
+    decrypted = dec(encrypted, *args)
     assert msg == decrypted
+    if verbose:
+        print(
+            f"{name} test case successfull\n"
+            f"result: {' -> '.join((msg, encrypted, decrypted))}"
+        )
     
+def bench_cypher(enc, dec, msg, *args, name="Cypher", n=10_000, r=3, verbose=False):
+    time_f = partial(time, n=n, r=r)
+    enc_f = partial(enc, msg, *args)
+    encrypted = enc_f()
+    dec_f = partial(dec, encrypted, *args)
+    decrypted = dec_f()
+    encryption_time_ms = time_f(enc_f) * 1000
+    decryption_time_ms = time_f(dec_f) * 1000
+    if verbose:
+        print(
+            f"{name} benchmark results:\n"
+            f"encryption: {encryption_time_ms:.2f}ms\t"
+            f"decryption: {decryption_time_ms:.2f}ms\n"
+            f"result: {' -> '.join((msg, encrypted, decrypted))}"
+        )
+    return encryption_time_ms, decryption_time_ms
+    
+def time(f, n=100_000, r=3):
+    return min(timeit.repeat(f, number=n, repeat=r)) / n
